e3eb9285505847cf5e3ee0136df0868767500ded
Nathanael Roviery <roviery@gmail.com>
2022-04-08 20:07:59 +0700

    fix soal2

--- soal2.R
+++ soal2.R
@@ -4,7 +4,6 @@ dbinom(4, 20, 0.2)
 #Nomor 2b
 peluang <- dbinom(1:20, 20, 0.2)
 data = data.frame(y=c(peluang), x=c(1:20))
-data
 barplot(data$y, names.arg=data$x, ylab="Peluang", xlab="Jumlah Pasien")
 
 #Nomor 2c
