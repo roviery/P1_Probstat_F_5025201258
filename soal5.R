a46a0aceaeb93872b45dfe412fdaabf9e660c68f
Nathanael Roviery <roviery@gmail.com>
2022-04-08 21:00:52 +0700

    soal 5

---
+++ soal5.R
@@ -0,0 +1,19 @@
+#Nomor 5a
+dexp(3)
+
+#Nomor 5b
+set.seed(1)
+hist(rexp(10))
+set.seed(1)
+hist(rexp(100))
+set.seed(1)
+hist(rexp(1000))
+set.seed(1)
+hist(rexp(10000))
+
+#Nomor 5c
+lambda = 3
+rataan = lambda
+rataan
+varian = lambda*lambda
+varian
